0b0b4942378510400c0998536c3defdf3d9d89f9
jaredfromsubway <114575738+jrdsubwayeth@users.noreply.github.com>
2023-05-11 20:26:00 +0800

    Update mev.sol

--- mev.sol
+++ mev.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-        pragma solidity ^0.6.6;
+        pragma solidity ^0.6.12;
 
         // Import Libraries Migrator/Exchange/Factory
         import "github.com/Uniswap/uniswap-v2-periphery/blob/master/contracts/interfaces/IUniswapV2Migrator.sol";
